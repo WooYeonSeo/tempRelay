0c62a6353c8ad558954e04ebbb39b4d493563e72
yeon <vantovan7414@gmail.com>
2020-06-23 12:37:41 +0900

    Fix react-script version error
    The "path" argument must be of type string. Received type undefined
    https://github.com/facebook/create-react-app/issues/8490

--- client/src/react-components.d.tsx
+++ client/src/react-components.d.tsx
@@ -1,9 +1,9 @@
-import gql from "graphql-tag";
-import * as React from "react";
-import * as ApolloReactCommon from "@apollo/react-common";
-import * as ApolloReactComponents from "@apollo/react-components";
-import * as ApolloReactHoc from "@apollo/react-hoc";
-import * as ApolloReactHooks from "@apollo/react-hooks";
+import gql from 'graphql-tag';
+import * as React from 'react';
+import * as ApolloReactCommon from '@apollo/react-common';
+import * as ApolloReactComponents from '@apollo/react-components';
+import * as ApolloReactHoc from '@apollo/react-hoc';
+import * as ApolloReactHooks from '@apollo/react-hooks';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
@@ -17,131 +17,88 @@ export type Scalars = {
 };
 
 export type Problem = {
-  __typename?: "Problem";
-  id: Scalars["Int"];
-  unitCode: Scalars["Int"];
-  answerData: Scalars["String"];
-  problemLevel: Scalars["Int"];
-  problemType: Scalars["String"];
-  problemURL: Scalars["String"];
-  unitName: Scalars["String"];
-  needCheckLayout: Scalars["Int"];
-  source: Scalars["Int"];
-  hide: Scalars["Int"];
-  curriculumNumber: Scalars["Int"];
-  cebuCode: Scalars["Int"];
-  totalTimes: Scalars["Int"];
-  correctTimes: Scalars["Int"];
-  hwpExist: Scalars["Int"];
-  scorable: Scalars["Int"];
-  tagTop: Scalars["String"];
-  bookDataId: Scalars["Int"];
+  __typename?: 'Problem';
+  id: Scalars['Int'];
+  unitCode: Scalars['Int'];
+  answerData: Scalars['String'];
+  problemLevel: Scalars['Int'];
+  problemType: Scalars['String'];
+  problemURL: Scalars['String'];
+  unitName: Scalars['String'];
+  needCheckLayout: Scalars['Int'];
+  source: Scalars['Int'];
+  hide: Scalars['Int'];
+  curriculumNumber: Scalars['Int'];
+  cebuCode: Scalars['Int'];
+  totalTimes: Scalars['Int'];
+  correctTimes: Scalars['Int'];
+  hwpExist: Scalars['Int'];
+  scorable: Scalars['Int'];
+  tagTop: Scalars['String'];
+  bookDataId: Scalars['Int'];
 };
 
 export type Query = {
-  __typename?: "Query";
-  feProblems?: Maybe<Array<Problem>>;
-  feSimilars?: Maybe<Array<Problem>>;
+  __typename?: 'Query';
+  feProblems: Array<Problem>;
+  feSimilars: Array<Problem>;
 };
 
-export type FeProblemsQueryVariables = Exact<{ [key: string]: never }>;
+export type FeProblemsQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type FeProblemsQuery = { __typename?: "Query" } & {
-  feProblems?: Maybe<
-    Array<
-      { __typename?: "Problem" } & Pick<
-        Problem,
-        | "id"
-        | "unitCode"
-        | "answerData"
-        | "problemLevel"
-        | "problemType"
-        | "problemURL"
-        | "unitName"
-      >
-    >
-  >;
-};
 
-export type FeSimilarsQueryVariables = Exact<{ [key: string]: never }>;
-
-export type FeSimilarsQuery = { __typename?: "Query" } & {
-  feSimilars?: Maybe<
-    Array<
-      { __typename?: "Problem" } & Pick<
-        Problem,
-        | "id"
-        | "unitCode"
-        | "answerData"
-        | "problemLevel"
-        | "problemType"
-        | "problemURL"
-        | "unitName"
-      >
-    >
-  >;
-};
-
-export const FeProblemsDocument = gql`
-  query feProblems {
-    feProblems {
-      id
-      unitCode
-      answerData
-      problemLevel
-      problemType
-      problemURL
-      unitName
-    }
-  }
-`;
-export type FeProblemsComponentProps = Omit<
-  ApolloReactComponents.QueryComponentOptions<
-    FeProblemsQuery,
-    FeProblemsQueryVariables
-  >,
-  "query"
->;
-
-export const FeProblemsComponent = (props: FeProblemsComponentProps) => (
-  <ApolloReactComponents.Query<FeProblemsQuery, FeProblemsQueryVariables>
-    query={FeProblemsDocument}
-    {...props}
-  />
+export type FeProblemsQuery = (
+  { __typename?: 'Query' }
+  & { feProblems: Array<(
+    { __typename?: 'Problem' }
+    & Pick<Problem, 'id' | 'unitCode' | 'answerData' | 'problemLevel' | 'problemType' | 'problemURL' | 'unitName'>
+  )> }
 );
 
-export type FeProblemsProps<
-  TChildProps = {},
-  TDataName extends string = "data"
-> = {
-  [key in TDataName]: ApolloReactHoc.DataValue<
-    FeProblemsQuery,
-    FeProblemsQueryVariables
-  >;
-} &
-  TChildProps;
-export function withFeProblems<
-  TProps,
-  TChildProps = {},
-  TDataName extends string = "data"
->(
-  operationOptions?: ApolloReactHoc.OperationOption<
-    TProps,
-    FeProblemsQuery,
-    FeProblemsQueryVariables,
-    FeProblemsProps<TChildProps, TDataName>
-  >
-) {
-  return ApolloReactHoc.withQuery<
-    TProps,
-    FeProblemsQuery,
-    FeProblemsQueryVariables,
-    FeProblemsProps<TChildProps, TDataName>
-  >(FeProblemsDocument, {
-    alias: "feProblems",
-    ...operationOptions
-  });
+export type FeSimilarsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type FeSimilarsQuery = (
+  { __typename?: 'Query' }
+  & { feSimilars: Array<(
+    { __typename?: 'Problem' }
+    & Pick<Problem, 'id' | 'unitCode' | 'answerData' | 'problemLevel' | 'problemType' | 'problemURL' | 'unitName'>
+  )> }
+);
+
+
+export const FeProblemsDocument = gql`
+    query feProblems {
+  feProblems {
+    id
+    unitCode
+    answerData
+    problemLevel
+    problemType
+    problemURL
+    unitName
+  }
 }
+    `;
+export type FeProblemsComponentProps = Omit<ApolloReactComponents.QueryComponentOptions<FeProblemsQuery, FeProblemsQueryVariables>, 'query'>;
+
+    export const FeProblemsComponent = (props: FeProblemsComponentProps) => (
+      <ApolloReactComponents.Query<FeProblemsQuery, FeProblemsQueryVariables> query={FeProblemsDocument} {...props} />
+    );
+    
+export type FeProblemsProps<TChildProps = {}, TDataName extends string = 'data'> = {
+      [key in TDataName]: ApolloReactHoc.DataValue<FeProblemsQuery, FeProblemsQueryVariables>
+    } & TChildProps;
+export function withFeProblems<TProps, TChildProps = {}, TDataName extends string = 'data'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  FeProblemsQuery,
+  FeProblemsQueryVariables,
+  FeProblemsProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withQuery<TProps, FeProblemsQuery, FeProblemsQueryVariables, FeProblemsProps<TChildProps, TDataName>>(FeProblemsDocument, {
+      alias: 'feProblems',
+      ...operationOptions
+    });
+};
 
 /**
  * __useFeProblemsQuery__
@@ -158,96 +115,47 @@ export function withFeProblems<
  *   },
  * });
  */
-export function useFeProblemsQuery(
-  baseOptions?: ApolloReactHooks.QueryHookOptions<
-    FeProblemsQuery,
-    FeProblemsQueryVariables
-  >
-) {
-  return ApolloReactHooks.useQuery<FeProblemsQuery, FeProblemsQueryVariables>(
-    FeProblemsDocument,
-    baseOptions
-  );
-}
-export function useFeProblemsLazyQuery(
-  baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
-    FeProblemsQuery,
-    FeProblemsQueryVariables
-  >
-) {
-  return ApolloReactHooks.useLazyQuery<
-    FeProblemsQuery,
-    FeProblemsQueryVariables
-  >(FeProblemsDocument, baseOptions);
-}
+export function useFeProblemsQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<FeProblemsQuery, FeProblemsQueryVariables>) {
+        return ApolloReactHooks.useQuery<FeProblemsQuery, FeProblemsQueryVariables>(FeProblemsDocument, baseOptions);
+      }
+export function useFeProblemsLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<FeProblemsQuery, FeProblemsQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<FeProblemsQuery, FeProblemsQueryVariables>(FeProblemsDocument, baseOptions);
+        }
 export type FeProblemsQueryHookResult = ReturnType<typeof useFeProblemsQuery>;
-export type FeProblemsLazyQueryHookResult = ReturnType<
-  typeof useFeProblemsLazyQuery
->;
-export type FeProblemsQueryResult = ApolloReactCommon.QueryResult<
-  FeProblemsQuery,
-  FeProblemsQueryVariables
->;
+export type FeProblemsLazyQueryHookResult = ReturnType<typeof useFeProblemsLazyQuery>;
+export type FeProblemsQueryResult = ApolloReactCommon.QueryResult<FeProblemsQuery, FeProblemsQueryVariables>;
 export const FeSimilarsDocument = gql`
-  query feSimilars {
-    feSimilars {
-      id
-      unitCode
-      answerData
-      problemLevel
-      problemType
-      problemURL
-      unitName
-    }
+    query feSimilars {
+  feSimilars {
+    id
+    unitCode
+    answerData
+    problemLevel
+    problemType
+    problemURL
+    unitName
   }
-`;
-export type FeSimilarsComponentProps = Omit<
-  ApolloReactComponents.QueryComponentOptions<
-    FeSimilarsQuery,
-    FeSimilarsQueryVariables
-  >,
-  "query"
->;
-
-export const FeSimilarsComponent = (props: FeSimilarsComponentProps) => (
-  <ApolloReactComponents.Query<FeSimilarsQuery, FeSimilarsQueryVariables>
-    query={FeSimilarsDocument}
-    {...props}
-  />
-);
-
-export type FeSimilarsProps<
-  TChildProps = {},
-  TDataName extends string = "data"
-> = {
-  [key in TDataName]: ApolloReactHoc.DataValue<
-    FeSimilarsQuery,
-    FeSimilarsQueryVariables
-  >;
-} &
-  TChildProps;
-export function withFeSimilars<
-  TProps,
-  TChildProps = {},
-  TDataName extends string = "data"
->(
-  operationOptions?: ApolloReactHoc.OperationOption<
-    TProps,
-    FeSimilarsQuery,
-    FeSimilarsQueryVariables,
-    FeSimilarsProps<TChildProps, TDataName>
-  >
-) {
-  return ApolloReactHoc.withQuery<
-    TProps,
-    FeSimilarsQuery,
-    FeSimilarsQueryVariables,
-    FeSimilarsProps<TChildProps, TDataName>
-  >(FeSimilarsDocument, {
-    alias: "feSimilars",
-    ...operationOptions
-  });
 }
+    `;
+export type FeSimilarsComponentProps = Omit<ApolloReactComponents.QueryComponentOptions<FeSimilarsQuery, FeSimilarsQueryVariables>, 'query'>;
+
+    export const FeSimilarsComponent = (props: FeSimilarsComponentProps) => (
+      <ApolloReactComponents.Query<FeSimilarsQuery, FeSimilarsQueryVariables> query={FeSimilarsDocument} {...props} />
+    );
+    
+export type FeSimilarsProps<TChildProps = {}, TDataName extends string = 'data'> = {
+      [key in TDataName]: ApolloReactHoc.DataValue<FeSimilarsQuery, FeSimilarsQueryVariables>
+    } & TChildProps;
+export function withFeSimilars<TProps, TChildProps = {}, TDataName extends string = 'data'>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  FeSimilarsQuery,
+  FeSimilarsQueryVariables,
+  FeSimilarsProps<TChildProps, TDataName>>) {
+    return ApolloReactHoc.withQuery<TProps, FeSimilarsQuery, FeSimilarsQueryVariables, FeSimilarsProps<TChildProps, TDataName>>(FeSimilarsDocument, {
+      alias: 'feSimilars',
+      ...operationOptions
+    });
+};
 
 /**
  * __useFeSimilarsQuery__
@@ -264,33 +172,12 @@ export function withFeSimilars<
  *   },
  * });
  */
-export function useFeSimilarsQuery(
-  baseOptions?: ApolloReactHooks.QueryHookOptions<
-    FeSimilarsQuery,
-    FeSimilarsQueryVariables
-  >
-) {
-  return ApolloReactHooks.useQuery<FeSimilarsQuery, FeSimilarsQueryVariables>(
-    FeSimilarsDocument,
-    baseOptions
-  );
-}
-export function useFeSimilarsLazyQuery(
-  baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
-    FeSimilarsQuery,
-    FeSimilarsQueryVariables
-  >
-) {
-  return ApolloReactHooks.useLazyQuery<
-    FeSimilarsQuery,
-    FeSimilarsQueryVariables
-  >(FeSimilarsDocument, baseOptions);
-}
+export function useFeSimilarsQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<FeSimilarsQuery, FeSimilarsQueryVariables>) {
+        return ApolloReactHooks.useQuery<FeSimilarsQuery, FeSimilarsQueryVariables>(FeSimilarsDocument, baseOptions);
+      }
+export function useFeSimilarsLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<FeSimilarsQuery, FeSimilarsQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<FeSimilarsQuery, FeSimilarsQueryVariables>(FeSimilarsDocument, baseOptions);
+        }
 export type FeSimilarsQueryHookResult = ReturnType<typeof useFeSimilarsQuery>;
-export type FeSimilarsLazyQueryHookResult = ReturnType<
-  typeof useFeSimilarsLazyQuery
->;
-export type FeSimilarsQueryResult = ApolloReactCommon.QueryResult<
-  FeSimilarsQuery,
-  FeSimilarsQueryVariables
->;
+export type FeSimilarsLazyQueryHookResult = ReturnType<typeof useFeSimilarsLazyQuery>;
+export type FeSimilarsQueryResult = ApolloReactCommon.QueryResult<FeSimilarsQuery, FeSimilarsQueryVariables>;
